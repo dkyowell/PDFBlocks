ca0294877a4737657f90ca7555fd1e699667ec6d
David Yowell <dkyowell.opensource@gmail.com>
2024-04-02 14:03:05 -0500

    docs: add example of Table usage

--- Sources/PDFBlocks/Examples/ExampleSupport.swift
+++ Sources/PDFBlocks/Examples/ExampleSupport.swift
@@ -1,8 +1,39 @@
-//
-//  File.swift
-//  
-//
-//  Created by David Yowell on 4/2/24.
-//
+/**
+ *  PDF Blocks
+ *  Copyright (c) David Yowell 2024
+ *  MIT license, see LICENSE file for details
+ */
 
 import Foundation
+
+extension FormatStyle where Self == IntegerFormatStyle<Int> {
+
+    static var nogroup: IntegerFormatStyle<Int> {
+        .number
+        .grouping(.never)
+    }
+}
+
+extension FormatStyle where Self == Date.FormatStyle {
+
+    static var mmddyy: Date.FormatStyle {
+        Date.FormatStyle()
+            .month(.defaultDigits)
+            .day(.defaultDigits)
+            .year(.twoDigits)
+    }
+}
+
+func loadData<T>(_ type: T.Type, from: String) -> [T] where T: Decodable {
+    guard let data = from.data(using: .utf8) else {
+        return []
+    }
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+    let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .formatted(dateFormatter)
+    guard let result = try? decoder.decode([T].self, from: data) else {
+        return []
+    }
+    return result
+}
