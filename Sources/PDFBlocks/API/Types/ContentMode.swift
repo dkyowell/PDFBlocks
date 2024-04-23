e189ab6c5e5ecf4fcff1e57058be9336156f515a
David Yowell <dkyowell.opensource@gmail.com>
2024-04-22 20:02:24 -0500

    feat: add ContentMode (not fully implemented yet)

---
+++ Sources/PDFBlocks/API/Types/ContentMode.swift
@@ -0,0 +1,27 @@
+/**
+ *  PDF Blocks
+ *  Copyright (c) David Yowell 2024
+ *  MIT license, see LICENSE file for details
+ */
+
+import Foundation
+
+public enum ContentMode {
+    /// An option that resizes the content so it's all within the available space,
+    /// both vertically and horizontally.
+    ///
+    /// This mode preserves the content's aspect ratio.
+    /// If the content doesn't have the same aspect ratio as the available
+    /// space, the content becomes the same size as the available space on
+    /// one axis and leaves empty space on the other.
+    case fit
+
+    /// An option that resizes the content so it occupies all available space,
+    /// both vertically and horizontally.
+    ///
+    /// This mode preserves the content's aspect ratio.
+    /// If the content doesn't have the same aspect ratio as the available
+    /// space, the content becomes the same size as the available space on
+    /// one axis, and larger on the other axis.
+    // case fill
+}
