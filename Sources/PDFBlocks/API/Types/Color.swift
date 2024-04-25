db9998c38b5bf9bc4073bc4863f54d1ff0cc13df
David Yowell <dkyowell.opensource@gmail.com>
2024-04-24 19:25:29 -0500

    fix: move Color struct to correct file

--- Sources/PDFBlocks/API/Types/Color.swift
+++ Sources/PDFBlocks/API/Types/Color.swift
@@ -6,7 +6,23 @@
 
 import Foundation
 
-struct TextStroke {
-    let color: Color
-    let lineWidth: Dimension
+import Foundation
+
+/// Color is both a representation for color generally and a block for rendering
+/// a rectangular region of color.
+///
+/// To set the foregroundColor for a block:
+///
+///     Text("Hello, world.")
+///         .foregroundColor(Color.red)
+///
+/// To draw a blue square:
+///
+///     Color(UIColor.blue)
+///         .frame(width: .in(1), height: .in(1))
+///
+/// Each platform (iOS/macOS/Linux) uses its own color type that conforms
+/// to PlatformColor.
+public struct Color {
+    let platformColor: any PlatformColor
 }
