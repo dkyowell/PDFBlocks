23f6fe62b20241e05bae07bb23c511d26af6906f
David Yowell <dkyowell.opensource@gmail.com>
2024-04-28 09:06:14 -0500

    fix: if/else and switch functionality

---
+++ Sources/PDFBlocks/API/Blocks/EitherBlock.swift
@@ -0,0 +1,21 @@
+//
+//  EitherBlock.swift
+//
+//
+//  Created by David Yowell on 4/28/24.
+//
+
+import Foundation
+
+/// A special block used by `@BlockBuilder` for constructing
+/// blocks with if/else and switch statements. `EitherBlock`
+/// cannot be constructed directly.
+
+public struct EitherBlock<TrueContent, FalseContent>: Block where TrueContent: Block, FalseContent: Block {
+    let value: Value
+
+    enum Value {
+        case trueContent(TrueContent)
+        case falseContent(FalseContent)
+    }
+}
---
+++ Sources/PDFBlocks/Internal/Blocks/EitherBlock+Renderable.swift
@@ -0,0 +1,32 @@
+//
+//  EitherBlock+Renderable.swift
+//
+//
+//  Created by David Yowell on 4/28/24.
+//
+
+import Foundation
+
+extension EitherBlock: Renderable {
+    func sizeFor(context: Context, environment: EnvironmentValues, proposal: Proposal) -> BlockSize {
+        switch value {
+        case let .trueContent(content):
+            let node = content.getRenderable(environment: environment)
+            return node.sizeFor(context: context, environment: environment, proposal: proposal)
+        case let .falseContent(content):
+            let node = content.getRenderable(environment: environment)
+            return node.sizeFor(context: context, environment: environment, proposal: proposal)
+        }
+    }
+
+    func render(context: Context, environment: EnvironmentValues, rect: CGRect) -> (any Renderable)? {
+        switch value {
+        case let .trueContent(content):
+            let node = content.getRenderable(environment: environment)
+            return node.render(context: context, environment: environment, rect: rect)
+        case let .falseContent(content):
+            let node = content.getRenderable(environment: environment)
+            return node.render(context: context, environment: environment, rect: rect)
+        }
+    }
+}
