00a2b00fb7f0121a2656cdb1b8df31d5931a4416
David Yowell <dkyowell.opensource@gmail.com>
2024-05-04 13:09:54 -0500

    feat: add secondary pageWrap rendering to Columns

--- Sources/PDFBlocks/Internal/Blocks/Columns+Renderable.swift
+++ Sources/PDFBlocks/Internal/Blocks/Columns+Renderable.swift
@@ -6,7 +6,6 @@
 
 import Foundation
 
-// TODO: .secondary treated as atomic at present
 extension Columns: Renderable {
     func getTrait<Value>(context _: Context, environment _: EnvironmentValues, keypath: KeyPath<Trait, Value>) -> Value {
         Trait(allowWrap: pageWrap)[keyPath: keypath]
@@ -16,7 +15,7 @@ extension Columns: Renderable {
         var environment = environment
         environment.layoutAxis = .vertical
         switch wrapMode(context: context, environment: environment) {
-        case .none, .secondary:
+        case .none:
             let columnWidth = (proposal.width - CGFloat(count - 1) * spacing.points) / CGFloat(count)
             let blockProposal = CGSize(width: columnWidth, height: .infinity)
             let blocks = content.getRenderables(environment: environment)
@@ -25,6 +24,15 @@ extension Columns: Renderable {
             let height = heightForEvenColumns(sizes: sizes, firstColumnCount: nil)
             return BlockSize(min: CGSize(width: proposal.width, height: height),
                              max: CGSize(width: proposal.width, height: height))
+        case .secondary:
+            let columnWidth = (proposal.width - CGFloat(count - 1) * spacing.points) / CGFloat(count)
+            let blockProposal = CGSize(width: columnWidth, height: .infinity)
+            let blocks = content.getRenderables(environment: environment)
+                .flatMap { $0.decompose(context: context, environment: environment, proposal: blockProposal) }
+            let sizes = blocks.map { $0.sizeFor(context: context, environment: environment, proposal: blockProposal) }
+            let height = heightForEvenColumns(sizes: sizes, firstColumnCount: nil)
+            return BlockSize(min: CGSize(width: proposal.width, height: min(height, proposal.height)),
+                             max: CGSize(width: proposal.width, height: min(height, proposal.height)))
         case .primary:
             return BlockSize(min: .init(width: proposal.width, height: 0), max: proposal)
         }
@@ -32,18 +40,21 @@ extension Columns: Renderable {
 
     func render(context: Context, environment: EnvironmentValues, rect: CGRect) -> (any Renderable)? {
         var environment = environment
+        environment.textContinuationMode = true
         environment.layoutAxis = .vertical
         switch wrapMode(context: context, environment: environment) {
-        case .none, .secondary:
-            renderColumnsAtomic2(context: context, environment: environment, rect: rect)
+        case .none:
+            renderAtomic(context: context, environment: environment, rect: rect)
             return nil
+        case .secondary:
+            return renderSecondary(context: context, environment: environment, rect: rect)
         case .primary:
             context.renderer.setLayer(2)
             context.setPageWrapRect(rect)
             if context.multiPagePass == nil {
                 context.multiPagePass = {
                     environment.renderMode = .wrapping
-                    renderColumnsPrimary(context: context, environment: environment, rect: rect)
+                    renderPrimary(context: context, environment: environment, rect: rect)
                 }
             }
             return nil
@@ -52,24 +63,107 @@ extension Columns: Renderable {
 }
 
 extension Columns {
-    func renderColumnsAtomic(context: Context, environment: EnvironmentValues, rect: CGRect) {
+    func renderAtomic(context: Context, environment: EnvironmentValues, rect: CGRect) {
         let columnWidth = (rect.width - CGFloat(count - 1) * spacing.points) / CGFloat(count)
-        let blockSize = CGSize(width: columnWidth, height: .infinity)
-        let blocks = content.getRenderables(environment: environment)
-            .map { $0.decompose(context: context, environment: environment, proposal: blockSize) }
-            .flatMap { $0 }
-        var dy = 0.0
-        var column = 0
-        for block in blocks {
-            let size = block.sizeFor(context: context, environment: environment, proposal: blockSize)
-            let dx = CGFloat(column) * (columnWidth + spacing.points)
-            let renderRect = CGRect(origin: rect.origin.offset(dx: dx, dy: dy), size: size.min)
-            block.render(context: context, environment: environment, rect: renderRect)
-            dy += size.min.height
-            if dy ~>= rect.height {
-                column += 1
-                dy = 0
+        var blocks = content.getRenderables(environment: environment)
+        while blocks.count > 0 {
+            for column in 0 ..< count {
+                let xPos = rect.minX + (columnWidth + spacing.points) * CGFloat(column)
+                var dy: CGFloat = 0
+                while rect.minY + dy < rect.maxY, blocks.isEmpty == false {
+                    guard let block = blocks.first else {
+                        break
+                    }
+                    blocks = blocks.dropFirst().map { $0 }
+                    let remainingSize = CGSize(width: columnWidth, height: rect.height - dy)
+                    let size = block.sizeFor(context: context, environment: environment, proposal: remainingSize)
+                    if dy + size.min.height ~<= rect.height {
+                        let renderRect = CGRect(x: xPos, y: rect.minY + dy, width: columnWidth, height: size.max.height)
+                        let remainder = block.render(context: context, environment: environment, rect: renderRect)
+                        dy += renderRect.height
+                        if let remainder {
+                            blocks.insert(remainder, at: 0)
+                            break
+                        }
+                    } else {
+                        blocks.insert(block, at: 0)
+                        break
+                    }
+                }
             }
+        }
+    }
+
+    func renderPrimary(context: Context, environment: EnvironmentValues, rect: CGRect) {
+        let columnWidth = (rect.width - CGFloat(count - 1) * spacing.points) / CGFloat(count)
+        var blocks = content.getRenderables(environment: environment)
+        while blocks.count > 0 {
+            for column in 0 ..< count {
+                let xPos = rect.minX + (columnWidth + spacing.points) * CGFloat(column)
+                var dy: CGFloat = 0
+                while rect.minY + dy < rect.maxY, blocks.isEmpty == false {
+                    guard let block = blocks.first else {
+                        break
+                    }
+                    blocks = blocks.dropFirst().map { $0 }
+                    let remainingSize = CGSize(width: columnWidth, height: rect.height - dy)
+                    let size = block.sizeFor(context: context, environment: environment, proposal: remainingSize)
+                    if rect.minY + dy + size.min.height < rect.maxY {
+                        let renderRect = CGRect(x: xPos, y: rect.minY + dy, width: columnWidth, height: size.max.height)
+                        let remainder = block.render(context: context, environment: environment, rect: renderRect)
+                        dy += renderRect.height
+                        if let remainder {
+                            blocks.insert(remainder, at: 0)
+                            break
+                        }
+                    } else {
+                        blocks.insert(block, at: 0)
+                        break
+                    }
+                }
+            }
+            if pageWrap {
+                if blocks.count > 0 {
+                    context.endPage()
+                    context.beginPage()
+                }
+            } else {
+                return
+            }
+        }
+    }
+
+    func renderSecondary(context: Context, environment: EnvironmentValues, rect: CGRect) -> (any Renderable)? {
+        let columnWidth = (rect.width - CGFloat(count - 1) * spacing.points) / CGFloat(count)
+        var blocks = content.getRenderables(environment: environment)
+        for column in 0 ..< count {
+            let xPos = rect.minX + (columnWidth + spacing.points) * CGFloat(column)
+            var dy: CGFloat = 0
+            while rect.minY + dy < rect.maxY, blocks.isEmpty == false {
+                guard let block = blocks.first else {
+                    break
+                }
+                blocks = blocks.dropFirst().map { $0 }
+                let remainingSize = CGSize(width: columnWidth, height: rect.height - dy)
+                let size = block.sizeFor(context: context, environment: environment, proposal: remainingSize)
+                if dy + size.min.height ~<= rect.height {
+                    let renderRect = CGRect(x: xPos, y: rect.minY + dy, width: columnWidth, height: size.max.height)
+                    let remainder = block.render(context: context, environment: environment, rect: renderRect)
+                    dy += renderRect.height
+                    if let remainder {
+                        blocks.insert(remainder, at: 0)
+                        break
+                    }
+                } else {
+                    blocks.insert(block, at: 0)
+                    break
+                }
+            }
+        }
+        if blocks.count > 0 {
+            return Columns<ArrayBlock>(count: count, spacing: spacing, pageWrap: pageWrap, content: { ArrayBlock(blocks: blocks) })
+        } else {
+            return nil
         }
     }
 
@@ -109,82 +203,6 @@ extension Columns {
         return workingHeight
     }
 
-    func renderColumnsPrimary(context: Context, environment: EnvironmentValues, rect: CGRect) {
-        var environment = environment
-        environment.textContinuationMode = true
-        let columnWidth = (rect.width - CGFloat(count - 1) * spacing.points) / CGFloat(count)
-        var blocks = content.getRenderables(environment: environment)
-        while blocks.count > 0 {
-            for column in 0 ..< count {
-                let xPos = rect.minX + (columnWidth + spacing.points) * CGFloat(column)
-                var dy: CGFloat = 0
-                while rect.minY + dy < rect.maxY, blocks.isEmpty == false {
-                    guard let block = blocks.first else {
-                        break
-                    }
-                    blocks = blocks.dropFirst().map { $0 }
-                    let remainingSize = CGSize(width: columnWidth, height: rect.height - dy)
-                    let size = block.sizeFor(context: context, environment: environment, proposal: remainingSize)
-                    if rect.minY + dy + size.min.height < rect.maxY {
-                        let renderRect = CGRect(x: xPos, y: rect.minY + dy, width: columnWidth, height: size.max.height)
-                        let remainder = block.render(context: context, environment: environment, rect: renderRect)
-                        dy += renderRect.height
-                        if let remainder {
-                            blocks.insert(remainder, at: 0)
-                            break
-                        }
-                    } else {
-                        blocks.insert(block, at: 0)
-                        break
-                    }
-                }
-            }
-            if pageWrap {
-                if blocks.count > 0 {
-                    context.endPage()
-                    context.beginPage()
-                }
-            } else {
-                return
-            }
-        }
-    }
-
-    func renderColumnsAtomic2(context: Context, environment: EnvironmentValues, rect: CGRect) {
-        var environment = environment
-        environment.textContinuationMode = true
-        let columnWidth = (rect.width - CGFloat(count - 1) * spacing.points) / CGFloat(count)
-        var blocks = content.getRenderables(environment: environment)
-        while blocks.count > 0 {
-            for column in 0 ..< count {
-                let xPos = rect.minX + (columnWidth + spacing.points) * CGFloat(column)
-                var dy: CGFloat = 0
-                while rect.minY + dy < rect.maxY, blocks.isEmpty == false {
-                    guard let block = blocks.first else {
-                        break
-                    }
-                    blocks = blocks.dropFirst().map { $0 }
-                    let remainingSize = CGSize(width: columnWidth, height: rect.height - dy)
-                    let size = block.sizeFor(context: context, environment: environment, proposal: remainingSize)
-                    print(size)
-                    if dy + size.min.height ~<= rect.height {
-                        let renderRect = CGRect(x: xPos, y: rect.minY + dy, width: columnWidth, height: size.max.height)
-                        print(renderRect)
-                        let remainder = block.render(context: context, environment: environment, rect: renderRect)
-                        dy += renderRect.height
-                        if let remainder {
-                            blocks.insert(remainder, at: 0)
-                            break
-                        }
-                    } else {
-                        blocks.insert(block, at: 0)
-                        break
-                    }
-                }
-            }
-        }
-    }
-
     func wrapMode(context _: Context, environment: EnvironmentValues) -> WrapMode {
         if pageWrap {
             if environment.renderMode == .wrapping {
@@ -197,5 +215,3 @@ extension Columns {
         }
     }
 }
-
-extension Columns {}
